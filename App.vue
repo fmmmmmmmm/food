2c3bb8dc4be264a0619e193e961cdeec57c2adf9
XjmXz <1040891766@qq.com>
2020-12-11 19:07:00 +0800

    项目第一天

---
+++ App.vue
@@ -0,0 +1,18 @@
+<script>
+	import './static/iconfont-weapp-icon.css';
+	export default {
+		onLaunch: function() {
+			console.log('App Launch')
+		},
+		onShow: function() {
+			console.log('App Show')
+		},
+		onHide: function() {
+			console.log('App Hide')
+		}
+	}
+</script>
+
+<style>
+	/*每个页面公共css */
+</style>
